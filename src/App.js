0b87b18d4cbd01db56b47dba4a9e3c48dfd29ef8
richam68 <richamhshwr2@gmail.com>
2024-02-13 16:45:20 +0530

    changes code

--- src/App.js
+++ src/App.js
@@ -12,10 +12,13 @@ function App() {
   const fetchApi = async () => {
     try {
       const response = await fetch("https://restcountries.com/v3.1/all");
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
       const data = await response.json();
       setCountryData(data);
     } catch (err) {
-      console.error('Error fetching data:', err);
+      console.log("Fetching error", err);
     }
   };
 
