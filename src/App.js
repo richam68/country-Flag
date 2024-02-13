0d2999872892b1480a02f671ab8f6bc1d6fa1007
richam68 <richamhshwr2@gmail.com>
2024-02-13 15:49:22 +0530

    change in code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Country from "./country";
+import { useSnackbar } from "notistack";
 
 function App() {
+  const { enqueueSnackbar } = useSnackbar();
   const [countryData, setCountryData] = useState([]);
 
   useEffect(() => {
@@ -14,8 +16,22 @@ function App() {
       const response = await fetch("https://restcountries.com/v3.1/all");
       const data = await response.json();
       setCountryData(data);
-    } catch (err) {
-      console.log("Some error occurred", err);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        enqueueSnackbar(
+          error.response.data.message,
+          {
+            variant: "error",
+          }
+        )
+      }else{
+        enqueueSnackbar(
+          "Something went wrong. Check that the backend is running, reachable and returns valid JSON.",
+          {
+            variant: "error",
+          }
+        )
+       } 
     }
   };
 
